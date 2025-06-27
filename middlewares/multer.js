7e9696d52f1a5753a326a7bbdb94e3c94f92dbbb
Melvin-James <melvinjamesk2003@gmail.com>
2025-06-27 10:56:35 +0530

    commit

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,32 +1,18 @@
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('../config/cloudinary');
 
-const getStorage = (folderName) => multer.diskStorage({
-  destination: function (req, file, cb) {
-    const fullPath = path.join(__dirname,'../public/uploads',folderName);
-    fs.mkdirSync(fullPath,{recursive:true});
-    console.log('Uploading to:',fullPath);
-    cb(null,fullPath);
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext = path.extname(file.originalname);
-    cb(null, 'userImage-' + uniqueSuffix + ext);
-  }
-});
+const getCloudStorage = (folderName) =>
+  new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: `bookly/${folderName}`,
+      allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
+    },
+  });
 
-
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp'];
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error('Only JPEG, PNG, JPG, WEBP images are allowed'), false);
-  }
-};
-
-
-const uploadTo = (folderName) => multer({ storage: getStorage(folderName), fileFilter });
+const uploadTo = (folderName) =>
+  multer({ storage: getCloudStorage(folderName) });
 
 module.exports = uploadTo;
+
